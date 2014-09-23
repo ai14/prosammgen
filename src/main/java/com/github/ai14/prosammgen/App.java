6e0a722c4a42ac744cd998a7951970394fcc937c
David Flemström <david.flemstrom@gmail.com>
2014-09-23 18:00:49 +0200

    Set up intial Maven project

---
+++ src/main/java/com/github/ai14/prosammgen/App.java
@@ -0,0 +1,7 @@
+package com.github.ai14.prosammgen;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/github/ai14/prosammgen/AppTest.java
@@ -0,0 +1,14 @@
+package com.github.ai14.prosammgen;
+
+import org.junit.Test;
+
+/**
+ * Useless unit test
+ * TODO: add better tests
+ */
+public class AppTest {
+    @Test
+    public void testNothing() {
+        assertTrue(true);
+    }
+}
