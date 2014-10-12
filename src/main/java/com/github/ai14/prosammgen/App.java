50d0083a730dc5fd839f37ca8b404b34ae899ce0
John Pertoft <john@pertoft.se>
2014-10-12 16:07:50 +0200

    ngram markov chain, contains a bug

--- src/main/java/com/github/ai14/prosammgen/App.java
+++ src/main/java/com/github/ai14/prosammgen/App.java
@@ -1,7 +1,17 @@
 package com.github.ai14.prosammgen;
 
+import java.io.File;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      File[] files = new File[args.length];
+      for (int i = 0; i < files.length; i++) {
+        files[i] = new File(args[i]);
+      }
+
+      TextGenerator textGen = new TextGenerator(files);
+      String text = textGen.generateText();
+
+      System.out.println(text);
     }
 }
