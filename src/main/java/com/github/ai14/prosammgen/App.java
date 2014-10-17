73ffcc45207098039e57fa04fa9a493cfb4f4824
Carl Thomé <cthome@kth.se>
2014-10-17 16:47:58 +0200

    PDF generation working with cygwin as well

--- src/main/java/com/github/ai14/prosammgen/App.java
+++ src/main/java/com/github/ai14/prosammgen/App.java
@@ -3,12 +3,18 @@ package com.github.ai14.prosammgen;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
 
     // Read input
     //TODO Add word length limit and author name.
+    if (args.length == 0) {
+      System.err.println("prosammgen PREVIOUS_REFLECTION_DOCUMENT READING_MATERIAL QUESTIONS");
+      System.err.println("In order to generate a reflection document, start the program with the above arguments (paths to plain text files).");
+      System.exit(-1);
+    }
     File previousReflectionDocument = new File(args[0]);
     File readingMaterial = new File(args[1]);
     File questions = new File(args[2]);
@@ -24,13 +30,16 @@ public class App {
       PrintWriter out = new PrintWriter("reflectiondocument.tex");
       out.write(report);
       out.close();
-      //TODO Find pdftex.
-      String[] cmd = {"pdftex", "reflectiondocument.tex"};
-      Runtime.getRuntime().exec(cmd);
-      System.out.println("Successfully generated a reflection document as PDF.");
+      String[] cmd = {"pdftex", "'&pdflatex'", "reflectiondocument.tex"};
+      Process p = Runtime.getRuntime().exec(cmd);
+      if (p.getErrorStream().available() == 0)
+        System.out.println("Successfully generated a reflection document as PDF.");
+      else {
+        Scanner s = new Scanner(p.getErrorStream());
+        while (s.hasNext()) System.err.println(s.next());
+      }
     } catch (IOException e) {
-      System.out.println("Tip: Install pdftex to generate reflection documents as PDF.");
-      System.out.println();
+      System.err.println("Install pdflatex to generate reflection documents as PDF.");
       System.out.println(report);
     }
   }
