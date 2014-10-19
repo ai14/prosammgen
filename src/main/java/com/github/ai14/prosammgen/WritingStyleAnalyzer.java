3665366739dd4f17ebeb7d036b7977d42de14b34
nandezer <marchf25@gmail.com>
2014-10-19 22:20:45 +0200

    Update WritingStyleAnalyzer.java
    interface class, deleting the code
    (rewriting it in WAnalyzerS.java)

--- src/main/java/com/github/ai14/prosammgen/WritingStyleAnalyzer.java
+++ src/main/java/com/github/ai14/prosammgen/WritingStyleAnalyzer.java
@@ -17,59 +17,14 @@ interface WritingStyleAnalyzer {
    *
    * @return
    */
-  public double[] getSentenceLengthProbabilities(Path text){
-    //probs from the text
-    int numSentence = 0;
-    int beginT = 0;
-    list<int>sentenceSize;
-    //Read the text
-    for (String line : Files.readAllLines(file)) {
-        String[] words = line.split("\\s+");
-        for (int i = 0; i < words.length; i++) {
-          // find all sentences
-          if (beginT == 0 || words[i].endsWith(".")) {
-            beginT = 1;
-            int count = 0;
-            //Check distance of the sentence
-            while(!words[i].endsWith(".")){
-              cout ++;
-              ++i;
-            }
-            sentenceSize[count]++;
-            ++numSentence;
-          }
-        }
-      }
-      double[] prob = new double[sentenceSize.length];
-      for(int j = 0; j < prob.length; j++){
-        prob[j] = sentenceSize[j]/numSentence;
-      }
-      return prob;
-  }
+  public double[] getSentenceLengthProbabilities(Path text);
 
   /**
    * Get the probabilities for words of length [0..longest word in the text].
    *
    * @return
    */
-  public double[] getWordLengthProbabilities(Path text){
-    //probs from the text
-    list<int>wordSize;
-    //Read the text
-    for (String line : Files.readAllLines(file)) {
-        String[] words = line.split("\\s+");
-        for (int i = 0; i < words.length; i++) {
-          //word size
-          wordSize[words[i].length]++;
-        }
-      }
-      //calculate probs
-      double[] prob = new double[wordSize.length];
-      for(int j = 0; j < prob.length ;j++){
-        prob[j] = wordSize[j]/words.length;
-      }
-      return prob;
-  }
+  public double[] getWordLengthProbabilities(Path text);
 
   /**
    * Get the probabilities for number of sentences per paragraph from [0..most number of sentences in a paragraph in the text].
