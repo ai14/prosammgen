04c3eb0c95c61eaa31c1afe5e41e912e50e83c7b
Carl Thomé <cthome@kth.se>
2014-10-19 22:55:29 +0200

    Removed interfaces, simplified ReflectionDocumentGenerator

--- src/main/java/com/github/ai14/prosammgen/Wikipedia.java
+++ src/main/java/com/github/ai14/prosammgen/Wikipedia.java
@@ -15,35 +15,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class WikipediaArticles implements TextSource {
-  private String[] searchTerms;
-  private int requestsPerSearchterm;
+
+public class Wikipedia {
 
   /**
-   * Download plaintext article content from Wikipedia with the MediaWiki API.
+   * Download Wikipedia articles with the MediaWiki API and parse out article content as plaintext.
    *
    * @param articles
    * @param searchTerms
+   * @return
    */
-  public WikipediaArticles(int articles, String... searchTerms) {
-    this.searchTerms = searchTerms;
-    this.requestsPerSearchterm = 1 + articles / (1 + searchTerms.length);
+  public static List<Path> getArticles(int articles, String... searchTerms) {
+    int requestsPerSearchterm = 1 + articles / (1 + searchTerms.length);
     if (requestsPerSearchterm > 500) {
       throw new IllegalArgumentException("Searching for too many articles. Disallowed by the MediaWiki API.");
     }
-  }
 
-  @Override
-  public Path[] getTexts() {
-    List<Path> articles = new ArrayList<>();
+    List<Path> searchResults = new ArrayList<>();
     try {
-      for (String searchTerm : searchTerms) {
+      for (int i = 0; i < searchTerms.length; i++) {
+        String searchTerm = searchTerms[i];
 
-        // Use cached file instead, if fresh enough.
+        // Use cached file for search term instead, if fresh enough.
         Path p = Paths.get(searchTerm + ".txt");
         if (Files.exists(p))
           if (System.currentTimeMillis() - Files.getLastModifiedTime(p).toMillis() < 2592000000l) {
-            articles.add(p);
+            searchResults.add(p);
             continue;
           } else {
             Files.delete(p);
@@ -52,10 +49,11 @@ public class WikipediaArticles implements TextSource {
         // Fetch fresh Wikipedia articles.
         try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(searchTerm + ".txt")))) {
           int gaplimit = requestsPerSearchterm; // “Because excerpts generation can be slow the limit is capped at one whole-page extract.” Solution: do several requests. Sorry Wikipedia!
-          for (int i = 0; i < gaplimit; i++) {
-            String url = "http://en.wikipedia.org/w/api.php";
+          for (int j = 0; j < gaplimit; j++) {
+
             //TODO Use MediaWiki free-text search instead of gapfrom.
-            String query = "?format=xml&action=query&generator=allpages&gaplimit=" + gaplimit + "&gapfrom=" + UrlEscapers.urlPathSegmentEscaper().escape(searchTerm) + "&prop=extracts&exsectionformat=plain&explaintext&excontinue=" + i;
+            String url = "http://en.wikipedia.org/w/api.php";
+            String query = "?format=xml&action=query&generator=allpages&gaplimit=" + gaplimit + "&gapfrom=" + UrlEscapers.urlPathSegmentEscaper().escape(searchTerm) + "&prop=extracts&exsectionformat=plain&explaintext&excontinue=" + j;
 
             try (Scanner s = new Scanner(new URL(url + query).openStream())) {
 
@@ -66,8 +64,7 @@ public class WikipediaArticles implements TextSource {
               // Extract article content from the response.
               String content = response.replaceAll("\\<.*?\\>", ""); //TODO Don't strip actual text content surrounded by < and >.
               content = StringEscapeUtils.unescapeHtml4(content); // Convert HTML entities to unicode.
-              content = content.replaceAll("&|%|\\$|#|_|\\{|\\}|~|\\^|\\\\|\u2103", ""); // Strip LaTeX reserved characters.
-              content = content.replace("π", "_"); // Strip unsupported characters.
+              content = content.replaceAll("&|%|\\$|#|_|\\{|\\}|~|\\^|\\\\", ""); // Strip LaTeX reserved characters.
 
               //TODO Try to filter out meta data sections such as "External links".
 
@@ -76,13 +73,14 @@ public class WikipediaArticles implements TextSource {
             }
           }
         }
-        articles.add(Paths.get(searchTerm + ".txt"));
+        // Store resulting articles in a file for the search term.
+        searchResults.add(Paths.get(searchTerm + ".txt"));
       }
     } catch (IOException e) {
-      System.err.println("Wikipedia could not be retrieved.");
+      System.err.println("Wikipedia articles could not be retrieved.");
       System.exit(-1);
     }
 
-    return articles.toArray(new Path[0]);
+    return searchResults;
   }
 }
--- src/main/java/com/github/ai14/prosammgen/WordNet.java
+++ src/main/java/com/github/ai14/prosammgen/WordNet.java
@@ -9,31 +9,32 @@ import java.io.RandomAccessFile;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class WordNetSynonyms implements Synonyms {
+public class WordNet {
 
+  // Uses a local copy of Princeton's WordNet, a synonyms database.
   private static final String index = "res/wordnet.idx", data = "res/wordnet.dat";
 
-  @Override
-  public ImmutableList<String> getSynonyms(ImmutableList<String> words) {
-    ArrayList<String> synonyms = new ArrayList<>();
-    boolean capitalize =
-            Character.isUpperCase(
-                    words.get(0).charAt(0)); //TODO Improve capitalization when cross-referencing.
-    for (String w : words) {
-      synonyms.add(w); // The input words are always thought of as synonyms to themselves.
-    }
+  /**
+   * Get synonyms for the input words, including the input words.
+   *
+   * @param words
+   * @return
+   */
+  public static ImmutableList<String> getSynonyms(ImmutableList<String> words) {
+    List<String> synonyms = new ArrayList<>();
+    for (String w : words) synonyms.add(w); // The input words are always thought of as synonyms to themselves.
+
+    boolean capitalize = Character.isUpperCase(words.get(0).charAt(0)); //TODO Improve capitalization when cross-referencing.
 
     try {
-      // Lookup the word in the smaller index file to find the byte offsets for the word's synonyms in the data file.
+      // Lookup word in the index file to find the byte offsets for the word's synonyms in the data file.
       Queue<Long> offsets = new LinkedList<>();
       try (BufferedReader br = new BufferedReader(new FileReader(index))) {
         String line;
-        while ((line = br.readLine())
-                != null) { // TODO Lookup in the index smarter than reading through the file. Hashing, for example.
+        while ((line = br.readLine()) != null) { // TODO Lookup in the index smarter than reading through the file. Hashing, for example.
           String[] ss = line.split("\\|");
           if (ss[0].equals(words.get(0))) {
-            offsets.add(Long.parseLong(
-                    ss[1])); // TODO Is it correct that soon-to-be cross-referenced words can always be found in the results when looking up the first word?
+            offsets.add(Long.parseLong(ss[1])); // TODO Is it correct that soon-to-be cross-referenced words can always be found in the results when looking up the first word?
           }
         }
       }
@@ -41,25 +42,18 @@ public class WordNetSynonyms implements Synonyms {
       // If word exists, lookup synonyms.
       try (RandomAccessFile raf = new RandomAccessFile(data, "r")) {
         while (!offsets.isEmpty()) {
-          long offset = offsets.remove();
-          raf.seek(offset);
+          raf.seek(offsets.remove());
 
           // Data format:
           // word|no. of entries
           // (wordType)|synonym|synonym|synonym (related term)|synonym (generic term)|...
           List<String> candidates = new ArrayList<>();
           HashMap<String, Boolean> seen = new HashMap<>();
-          for (String w : words) {
-            seen.put(w, false);
-          }
+          for (String w : words) seen.put(w, false);
           int found = 0;
           int entries = Integer.parseInt(raf.readLine().split("\\|")[1]);
           for (int entry = 0; entry < entries; ++entry) {
             String[] fields = raf.readLine().split("\\|");
-            String
-                    wordType =
-                    fields[0]; // TODO Perhaps use NLP logic in the text generation? Verb, noun, adjective, etc. info is available here.
-
             for (int field = 1; field < fields.length; ++field) {
               if (fields[field].contains("(antonym)")) {
                 continue;
@@ -71,6 +65,8 @@ public class WordNetSynonyms implements Synonyms {
               }
             }
           }
+
+          // Only keep synonyms if all cross-reference words were seen.
           if (found == words.size()) {
             synonyms.addAll(candidates);
           }
