854ba30f73ce7a31ba8c7d74164844c1aeea48ad
nandezer <marchf25@gmail.com>
2014-10-19 21:45:50 +0200

    getSentenceLengthProbabilities function
    Get the probabilities for sentences of length [0..longest sentence in the text].

--- src/main/java/com/github/ai14/prosammgen/WritingStyleAnalyzer.java
+++ src/main/java/com/github/ai14/prosammgen/WritingStyleAnalyzer.java
@@ -1,6 +1,7 @@
 package com.github.ai14.prosammgen;
 
 import java.nio.file.Path;
+import java.nio.file.Files;
 
 interface WritingStyleAnalyzer {
 
@@ -16,26 +17,54 @@ interface WritingStyleAnalyzer {
    *
    * @return
    */
-  public double[] getSentenceLengthProbabilities();
+  public double[] getSentenceLengthProbabilities(Path text){
+	  //probs from the text
+	  int numSentence = 0;
+    int beginT = 0;
+    list<int>sentenceSize;
+	  for (String line : Files.readAllLines(file)) {
+        String[] words = line.split("\\s+");
+
+        for (int i = 0; i < words.length; i++) {
+          // find all sentences
+          if (beginT == 0 || words[i].endsWith(".")) {
+            beginT = 1;
+            int count = 0;
+            //Check distance of the sentence
+            while(!words[i].endsWith(".")){
+              cout ++;
+              ++i;
+            }
+            sentenceSize[count]++;
+            ++numSentence;
+          }
+        }
+      }
+      double[] prob = new double[sentenceSize.size()];
+      for(int j = 0; j < prob.size() ++j){
+        prob[j] = sentenceSize[j]/numSentence;
+      }
+      return prob;
+  }
 
   /**
    * Get the probabilities for words of length [0..longest word in the text].
    *
    * @return
    */
-  public double[] getWordLengthProbabilities();
+  public double[] getWordLengthProbabilities(Path text);
 
   /**
    * Get the probabilities for number of sentences per paragraph from [0..most number of sentences in a paragraph in the text].
    *
    * @return
    */
-  public double[] getSentencesPerParagraphProbabilities();
+  public double[] getSentencesPerParagraphProbabilities(Path text);
 
   /**
    * Get the probabilities for ratios (answer length / question length).
    *
    * @return
    */
-  public double[] getQuestionLengthToAnswerLengthRatioProbabilities();
+  public double[] getQuestionLengthToAnswerLengthRatioProbabilities(Path text);
 }
