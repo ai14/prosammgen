87e2cf60c0d68d533342076156980b6a4dc64500
nandezer <marchf25@gmail.com>
2014-10-20 01:43:58 +0200

    Update WAnalyzerS.java

--- src/main/java/com/github/ai14/prosammgen/WAnalyzerS.java
+++ src/main/java/com/github/ai14/prosammgen/WAnalyzerS.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
-class WAnalyzerS implements WritingStyleAnalyzer{
+class WAnalyzerS{
 
   /**
    * Given a text this calculates the probabilities for different metrics that describes the writing style of the text author.
