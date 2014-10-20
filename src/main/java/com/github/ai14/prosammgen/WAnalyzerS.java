b1260c1457ba2767cb0f54f37bcf195d5804bfc6
nandezer <marchf25@gmail.com>
2014-10-20 13:52:36 +0200

    Update WAnalyzerS.java

--- src/main/java/com/github/ai14/prosammgen/WAnalyzerS.java
+++ src/main/java/com/github/ai14/prosammgen/WAnalyzerS.java
@@ -182,7 +182,7 @@ private Path text;
   public double getMispellingWordsProbabilities(){ //TODO: take it off this class
   double prob;
   try{
-  	WordNetSynonyms WordSynonym = new WordNetSynonyms();
+  	Synonyms WordSynonym = new Synonyms();
   	//probs from the text
 	    int numSentences = 0;
 	    int misPellingWords = 0;
