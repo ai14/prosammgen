c7fe54bdb457f435599a556110c086898311a29f
nandezer <marchf25@gmail.com>
2014-10-20 17:01:20 +0200

    Update WAnalyzerS.java
    improvements both coding style-wise and functionality-wise

--- src/main/java/com/github/ai14/prosammgen/WAnalyzerS.java
+++ src/main/java/com/github/ai14/prosammgen/WAnalyzerS.java
@@ -33,7 +33,8 @@ private Path text;
    * @param text
    */
   public void analyze(Path text){
-    this.text = text;
+
+      this.text = text;
   }
 
   /**
@@ -43,42 +44,44 @@ private Path text;
    */
 @Override  
   public double[] getSentenceLengthProbabilities() {
-  	double[] prob = new double[0];
+  	double[] probabilities = new double[0];
   	try{
-  		 //probs from the text
-	    int numSentence = 0;
-	    int beginT = 0;
-	    
+	    int SentenceNumber = 0;
+        //Using this to also count the first sentece of the text
+	    int FirstSentence = 0;
 		List<Integer>sentenceSize = new ArrayList<>();
 	    //Read the text
 	    for (String line : Files.readAllLines(text)) {
+            //Spliting text in words.
 	        String[] words = line.split("\\s+");
 	        for (int i = 0; i < words.length; i++) {
 	          // find all sentences
-	          if (beginT == 0 || words[i].endsWith(".")) {
-	            beginT = 1;
-	            int count = 1;
-	            ++i;
-	            //Check distance of the sentence
+	          if (FirstSentence == 0 || words[i].endsWith(".")) {
+                  FirstSentence = 1;
+	              int count = 1;//To count the first word too.
+	              ++i;
+	            //Check words of the sentence
 	            while(!words[i].endsWith(".")){
 	              count ++;
 	              ++i;
 	            }
-	            int x = sentenceSize.get(count);
-	            x++;
-	            sentenceSize.set(count, x);
-	            ++numSentence;
+                //increasing quantity of sentence with "count" words.
+	            int sentenceSameLength = sentenceSize.get(count);
+                  sentenceSameLength++;
+	            sentenceSize.set(count, sentenceSameLength);
+	            ++SentenceNumber;
 	          }
 	        }
-	      }
-	      prob = new double [sentenceSize.size()];
-	      for(int j = 0; j < prob.length; j++){
-	        prob[j] = sentenceSize.get(j)/numSentence;
-	      }
+	    }
+        probabilities = new double [sentenceSize.size()];
+        //Calculating probabilities
+	    for(int j = 0; j < probabilities.length; j++){
+            probabilities[j] = sentenceSize.get(j)/SentenceNumber;
+	    }
   	}catch (IOException e) {
 	      System.err.println("Couldn't read the text");
     	} finally {
-      		return prob;
+      		return probabilities;
     	}
    
   }
@@ -89,31 +92,31 @@ private Path text;
    */
    @Override
   public double[] getWordLengthProbabilities(){
-  	double[] prob = new double[0];
+  	double[] probabilities = new double[0];
   	try{
-  		//probs from the text
 	    int textSize = 0;
 	    List<Integer>wordSize = new ArrayList<>();
 	    //Read the text
 	    for (String line : Files.readAllLines(text)) {
+            //getting all the words
 	        String[] words = line.split("\\s+");
 	        textSize = words.length;
 	        for (int i = 0; i < textSize; i++) {
-	          //word size        
-			  int x = wordSize.get(words[i].length());
-			  x++;
-			  wordSize.set(words[i].length(), x);
+	          //increasing the number of characters with words[i].length
+			  int WordsSameLength = wordSize.get(words[i].length());
+              WordsSameLength++;
+			  wordSize.set(words[i].length(), WordsSameLength);
 	        }
 	    }
-	    //calculate probs
-	    prob = new double[wordSize.size()];
-	    for(int j = 0; j < prob.length ;j++){
-	        prob[j] = wordSize.get(j)/textSize;
+        //Calculating probabilities
+        probabilities = new double[wordSize.size()];
+	    for(int j = 0; j < probabilities.length ;j++){
+            probabilities[j] = wordSize.get(j)/textSize;
 	    }
   	}catch (IOException e) {
 	      System.err.println("Couldn't read the text");
     	} finally {
-      		return prob;
+      		return probabilities;
     	}
   }
 
@@ -124,39 +127,39 @@ private Path text;
    */
    @Override
   public double[] getSentencesPerParagraphProbabilities(){
-  	double[] prob = new double[0];
+  	double[] probabilities = new double[0];
   	try{
-	  			//probs from the text
 		int numParagraph = 0;
 		List<Integer>SentencesParagraph = new ArrayList<>();
 		//Read the text
 		for (String line : Files.readAllLines(text)) {
-			//Split in paragraphs
+			//getting text per paragraph
 			String[] paragraphs = line.split("\r");
 			numParagraph = paragraphs.length;
 			//each paragraph
 			for (int i = 0; i < numParagraph; i++) {
 				//Split into words every paragraph
 				String[] worksParagraph = line.split("\\s+");
-				int countSP = 0;			
+				int countSentences = 0;
 				//Count number of sentence per paragraph
 				for (int j = 0; j < worksParagraph.length; j++) {
 					// find all sentences per paragraph
-					if (worksParagraph[j].endsWith("."))++countSP;
+					if (worksParagraph[j].endsWith("."))++countSentences;
 				}
-				int x = SentencesParagraph.get(countSP);
-				x++;
-				SentencesParagraph.set(countSP, x);
+                //increasing the times that a paragraph have "countSentences" sentences
+				int sentencesParagraph = SentencesParagraph.get(countSentences);
+                sentencesParagraph++;
+				SentencesParagraph.set(countSentences, sentencesParagraph);
 			}
-		}	
-		prob = new double [SentencesParagraph.size()];
-		for(int j = 0; j < prob.length; j++){
-			prob[j] = SentencesParagraph.get(j)/numParagraph;
+		}
+        probabilities = new double [SentencesParagraph.size()];
+		for(int j = 0; j < probabilities.length; j++){
+            probabilities[j] = SentencesParagraph.get(j)/numParagraph;
 		}
   	}catch (IOException e) {
 	      System.err.println("Couldn't read the text");
     	} finally {
-      		return prob;
+      		return probabilities;
     	}
   	
 
@@ -169,6 +172,7 @@ private Path text;
    */
    @Override
   public double[] getQuestionLengthToAnswerLengthRatioProbabilities(){
+       //TODO: Implement function
 	  double [] i= new double[2];
     return i;
   }
@@ -179,42 +183,42 @@ private Path text;
    * @return
    */
    @Override
-  public double getMispellingWordsProbabilities(){ //TODO: take it off this class
-  double prob = 0;
+  public double getMisspellingWordsProbabilities(){ //TODO: take it off this class
+  double probability = 0;
   try{
-  	WordNetSynonyms WordSynonym = new WordNetSynonyms();
-  	//probs from the text
-	    int numSentences = 0;
-	    int misPellingWords = 0;
+  	    WordNetSynonyms WordSynonym = new WordNetSynonyms();
+	    int TotalSentences = 0;
+	    int misspellingWords = 0;
 	    List<Integer>wordSize = new ArrayList<>();
 	    //Read the text
 	    for (String line : Files.readAllLines(text)) {
 	    	//split text in sentences
 	        String[] sentences = line.split("(?i)(?<=[.?!])\\S+(?=[a-z])");
-	        numSentences = sentences.length;
+	        TotalSentences = sentences.length;
 	        //each sentence
-	        for (int i = 0; i < numSentences; i++) {
-	        	List<String>Swords = new ArrayList<>();
+	        for (int i = 0; i < TotalSentences; i++) {
 		    	//split the sentence in words
 				String[] words = sentences[i].split("\\s+");
-				ImmutableList<String>SynWord = null;
-				ImmutableList<String>Synonym;
+				ImmutableList<String>WordToCheck = null;
+				ImmutableList<String>PossibleSynonym;
+                //per each word
 				for(int j = 0;j < words.length; ++j){
-                    SynWord.add(0, words[j]);
-					//check for synonyms
-					Synonym = WordSynonym.getSynonyms(SynWord); //TODO: not real name in master
-					//Check for misspelling words
-					if(Synonym.size() == 1 && (Synonym.get(0)) == words[j]) ++misPellingWords;
-					SynWord.remove(0);
+                    WordToCheck.add(0, words[j]);
+					//get synonyms
+                    PossibleSynonym = WordSynonym.getSynonyms(WordToCheck); //TODO: not this name in master
+					//if there is no synonym (only get the same word as output of the function means that the word is misspelled
+					if(PossibleSynonym.size() == 1 && (PossibleSynonym.get(0)) == words[j]) ++misspellingWords;
+                    //check one word at a time, after checking we remove it from the list
+                    WordToCheck.remove(0);
 				}
 	        }
 	    }
-	    //calculate probs
-	    prob = misPellingWords/numSentences;
+	    //calculate probability
+      probability = misspellingWords/TotalSentences;
 	  }catch (IOException e) {
 		      System.err.println("Couldn't read the text");
     	} finally {
-      		return prob;
+      		return probability;
     	}
   }
     
