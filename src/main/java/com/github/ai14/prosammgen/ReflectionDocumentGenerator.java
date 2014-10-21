a16cea4ac498f5d4e93f0b2cd56ca532705a7b7d
nandezer <marchf25@gmail.com>
2014-10-21 14:29:01 +0200

    Update ReflectionDocumentGenerator.java
    Hand Merging

--- src/main/java/com/github/ai14/prosammgen/ReflectionDocumentGenerator.java
+++ src/main/java/com/github/ai14/prosammgen/ReflectionDocumentGenerator.java
@@ -4,12 +4,14 @@ import com.github.ai14.prosammgen.textgen.*;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Ordering;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.ParseException;
+import java.util.List;
 import java.util.Random;
 import java.util.function.Function;
 
@@ -17,7 +19,6 @@ public final class ReflectionDocumentGenerator {
   private final ImmutableSet<String> stopWords;
   private final NLPModel nlp;
   private ImmutableMap<String, TextGenerator> generators;
-  private Synonyms synonyms;
 
   public ReflectionDocumentGenerator() throws IOException, ParseException {
 
@@ -26,9 +27,6 @@ public final class ReflectionDocumentGenerator {
 
     // Load NLP.
     nlp = NLPModel.loadFromDBs(Paths.get("res/en-sent.bin"), Paths.get("res/en-token.bin"), Paths.get("res/en-pos-maxent.bin"));
-
-    // Load synonyms database.
-    synonyms = new WordNetSynonyms();
 
     // Load grammar.
     generators = TextGenerators.parseGrammar(Files.readAllLines(Paths.get("res/grammar")));
@@ -48,16 +46,29 @@ public final class ReflectionDocumentGenerator {
       // Append the question to the report as a headline.
       report.append("\\section{").append(question).append("}");
 
-      // Determine keywords for the question.
+      // Create keyword generator for the current question.
       KeywordGenerator keywordGenerator = KeywordGenerator.withPOSParsing(nlp, stopWords, question);
 
-      // Generate a Markov chain for the current question.
-      MarkovChain markovChain = generateMarkovChain(question, readingMaterial);
+      // Get additional training data for the question from text sources (Wikipedia articles).
+      ImmutableSet<String> searchTerms = keywordGenerator.getWords();
+      String longestSearchTerm = Ordering.natural().onResultOf(String::length).max(searchTerms);
+      List<Path> wikipediaArticles = Wikipedia.getArticles(100, longestSearchTerm);
+
+      // Calculate the ratio of amount of reading material per additional training text.
+      long s = 0;
+      for (Path p : wikipediaArticles) s += Files.size(p);
+      double ratio = Files.size(readingMaterial) / (double) s;
+
+      // Generate and train a Markov chain for the current question.
+      MarkovTrainer markovTrainer = new MarkovTrainer();
+      int weight = (int) (0.5 / ratio); // aim for reading material to be 50% of wiki articles TODO Document properly.
+      markovTrainer.train(weight, readingMaterial);
+      markovTrainer.train(wikipediaArticles.toArray(new Path[wikipediaArticles.size()]));
 
       // Define grammar macros for the current question.
       ImmutableMap.Builder<String, Function<ImmutableList<String>, TextGenerator>> macroBuilder = ImmutableMap.builder();
-      macroBuilder.put("MARKOV", n -> new MarkovTextGenerator(markovChain, Integer.parseInt(n.get(0))));
-      macroBuilder.put("SYNONYM", words -> new SynonymGenerator(words, synonyms));
+      macroBuilder.put("MARKOV", n -> new MarkovTextGenerator(markovTrainer, Integer.parseInt(n.get(0))));
+      macroBuilder.put("SYNONYM", words -> new SynonymGenerator(words));
       macroBuilder.put("KEYWORD", x -> keywordGenerator);
 
       int remaining = wordCount / questions.size();
@@ -71,38 +82,11 @@ public final class ReflectionDocumentGenerator {
 
     report.append("\\end{document}");
 
-    return report.toString();
-  }
-
-  private MarkovChain generateMarkovChain(String question, Path readingMaterial) {
-    MarkovChain markovChain = new MarkovChain();
-
-    try {
-      // Get additional training data for the question from text sources (Wikipedia articles).
-      //TODO Don't call with all keywords, use only the longest for example (or whatever is emph{} in the report.
-      KeywordGenerator keywordGenerator = KeywordGenerator.withPOSParsing(nlp, stopWords, question);
-      ImmutableSet<String> searchTerms = keywordGenerator.getWords();
-      TextSource wa = new WikipediaArticles(100, searchTerms.toArray(new String[searchTerms.size()]));
-      Path[] trainingTexts = wa.getTexts();
-
-      // Calculate the ratio of amount of reading material per additional training text.
-      long waSize = 0, rmSize = Files.size(readingMaterial);
-      for (Path p : trainingTexts) waSize += Files.size(p);
-      int weight = (int) (0.5 / (rmSize / ((double) waSize)));
-
-      // Train the Markov chain.
-      markovChain.train(weight, readingMaterial); // aim for reading material to be 50% of wiki articles TODO Document properly.
-      markovChain.train(trainingTexts);
-    } catch (IOException e) {
-      System.err.println("Could not train a Markov chain.");
-      System.exit(-1);
-    }
-
-    return markovChain;
+    return report.toString().replace("π|℃", "_"); // TODO Support these characters.
   }
 
   private static class SimpleContext implements TextGenerator.Context {
-    private static final Random random = new Random();
+    private final Random random = new Random();
     private final ImmutableMap<String, TextGenerator> generators;
     private final ImmutableMap<String, Function<ImmutableList<String>, TextGenerator>> macros;
 
