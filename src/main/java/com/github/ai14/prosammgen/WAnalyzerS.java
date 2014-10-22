3127c4f5e908514a5bc7eea67d53c1290d4cf7a3
nandezer <marchf25@gmail.com>
2014-10-22 03:33:43 +0200

    Update WAnalyzerS.java
    Adding camelCase.

--- src/main/java/com/github/ai14/prosammgen/WAnalyzerS.java
+++ src/main/java/com/github/ai14/prosammgen/WAnalyzerS.java
@@ -6,18 +6,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import com.google.common.collect.ImmutableList;
-import com.github.ai14.prosammgen.*;
-
 
 
 public class WAnalyzerS{
 
     private Path text;
-    private String[] TotalWords;
-    private String[] TotalSentences;
-    private String[] TotalParagraphs;
-    private List<String> Words;
+    private String[] totalWords;
+    private String[] totalSentences;
+    private String[] totalParagraphs;
+    private List<String> words;
  
   /**
    * Given a text this calculates the probabilities for different metrics that describes the writing style of the text author.
@@ -26,12 +23,12 @@ public class WAnalyzerS{
    */
   public void analyze(Path text){
     try {
-        this.Words = Files.readAllLines(Paths.get("res/words"));
+        this.words = Files.readAllLines(Paths.get("res/words"));
         this.text = text;
         String allText = new String(Files.readAllBytes(text));
-        this.TotalWords = allText.split("\\s+"); //splitting into words
-        this.TotalSentences = allText.split("(?i)(?<=[.?!])\\S+(?=[a-z])");//splitting into sentences
-        this.TotalParagraphs = allText.split("\\n\\n");//splitting into Paragraphs
+        this.totalWords = allText.split("\\s+"); //splitting into words
+        this.totalSentences = allText.split("(?i)(?<=[.?!])\\S+(?=[a-z])");//splitting into sentences
+        this.totalParagraphs = allText.split("\\n\\n");//splitting into Paragraphs
 
     }catch (IOException e) {
         System.err.println("Couldn't read the text");
@@ -45,11 +42,11 @@ public class WAnalyzerS{
    */
 
   public double[] getSentenceLengthProbabilities() {
-	    int NumberOfSentences = TotalSentences.length;
+	    int numberOfSentences = totalSentences.length;
 		List<Integer>sentenceSize = new ArrayList<>();
-        for (int i = 0; i < NumberOfSentences; i++) {
+        for (int i = 0; i < numberOfSentences; i++) {
             //split sentences into words
-            String[] words = TotalSentences[i].split("\\s+");
+            String[] words = totalSentences[i].split("\\s+");
             int sentenceSameLength = sentenceSize.get(words.length);
             sentenceSameLength++;
             sentenceSize.set(words.length, sentenceSameLength);
@@ -57,7 +54,7 @@ public class WAnalyzerS{
         double[] probabilities = new double [sentenceSize.size()];
         //Calculating probabilities
 	    for(int j = 0; j < probabilities.length; j++){
-            probabilities[j] = sentenceSize.get(j)/NumberOfSentences;
+            probabilities[j] = sentenceSize.get(j)/numberOfSentences;
 	    }
         return probabilities;
   }
@@ -70,12 +67,12 @@ public class WAnalyzerS{
   public double[] getWordLengthProbabilities(){
 	    int textSize = 0;
 	    List<Integer>wordSize = new ArrayList<>();
-        textSize = TotalWords.length;
+        textSize = totalWords.length;
         for (int i = 0; i < textSize; i++) {
           //increasing the number of characters with words[i].length
-          int WordsSameLength = wordSize.get(TotalWords[i].length());
-          WordsSameLength++;
-          wordSize.set(TotalWords[i].length(), WordsSameLength);
+          int wordsSameLength = wordSize.get(totalWords[i].length());
+          wordsSameLength++;
+          wordSize.set(totalWords[i].length(), wordsSameLength);
         }
         //Calculating probabilities
         double[] probabilities = new double[wordSize.size()];
@@ -93,21 +90,21 @@ public class WAnalyzerS{
 
   public double[] getSentencesPerParagraphProbabilities(){
 
-        int NumberOfParagraph = TotalParagraphs.length;
-        List<Integer>SentencesParagraph = new ArrayList<>();
+        int numberOfParagraph = totalParagraphs.length;
+        List<Integer>sentencesParagraph = new ArrayList<>();
         //each paragraph
-        for (int i = 0; i < NumberOfParagraph; i++) {
+        for (int i = 0; i < numberOfParagraph; i++) {
             //Split into sentences every paragraph
-            String[] SentencesOnParagraph = TotalParagraphs[i].split("(?i)(?<=[.?!])\\S+(?=[a-z])");
+            String[] sentencesOnParagraph = totalParagraphs[i].split("(?i)(?<=[.?!])\\S+(?=[a-z])");
             //increasing the times that a paragraph have "countSentences" sentences
-            int sentencesParagraph = SentencesParagraph.get(TotalParagraphs.length);
-            sentencesParagraph++;
-            SentencesParagraph.set(TotalParagraphs.length, sentencesParagraph);
+            int numberSentencesPerParagraphs = sentencesParagraph.get(totalParagraphs.length);
+            numberSentencesPerParagraphs++;
+            sentencesParagraph.set(totalParagraphs.length, numberSentencesPerParagraphs);
         }
 
-        double[] probabilities = new double [SentencesParagraph.size()];
+        double[] probabilities = new double [sentencesParagraph.size()];
         for(int j = 0; j < probabilities.length; j++){
-            probabilities[j] = SentencesParagraph.get(j)/NumberOfParagraph;
+            probabilities[j] = sentencesParagraph.get(j)/numberOfParagraph;
         }
         return probabilities;
 
@@ -121,28 +118,27 @@ public class WAnalyzerS{
    */
 
   public double getMisspellingWordsProbabilities(){
-        int NumberOfWords = TotalWords.length;
-        WordNet CheckSynonym = new WordNet();
+        int numberOfWords = totalWords.length;
 	    int misspellingWords = 0;
-	    List<Integer>MisspellWords = new ArrayList<>();;
-      //each word
-        for (int j = 0; j < NumberOfWords; j++) {
+	    List<Integer>misspellWords = new ArrayList<>();;
+        //for each word
+        for (int j = 0; j < numberOfWords; j++) {
             //If it's end of sentence, remove the last character to check the synonyms.
-            if ((TotalWords[j].endsWith(".") || TotalWords[j].endsWith("?") || TotalWords[j].endsWith("!")|| TotalWords[j].endsWith(",")|| TotalWords[j].endsWith(":") || TotalWords[j].endsWith(")")|| TotalWords[j].endsWith("”")|| TotalWords[j].endsWith(" \" ")) && TotalWords[j].length() > 0 && TotalWords[j] != null) {
-                TotalWords[j] = TotalWords[j].substring(0, TotalWords[j].length() - 1);
+            if ((totalWords[j].endsWith(".") || totalWords[j].endsWith("?") || totalWords[j].endsWith("!")|| totalWords[j].endsWith(",")|| totalWords[j].endsWith(":") || totalWords[j].endsWith(")")|| totalWords[j].endsWith("”")|| totalWords[j].endsWith(" \" ")) && totalWords[j].length() > 0 && totalWords[j] != null) {
+                totalWords[j] = totalWords[j].substring(0, totalWords[j].length() - 1);
             }
-            if ((TotalWords[j].startsWith("(")||TotalWords[j].startsWith("“")||TotalWords[j].startsWith("\""))  && TotalWords[j].length() > 0 && TotalWords[j] != null) {
-                TotalWords[j] = TotalWords[j].substring(1);
+            if ((totalWords[j].startsWith("(")|| totalWords[j].startsWith("“")|| totalWords[j].startsWith("\""))  && totalWords[j].length() > 0 && totalWords[j] != null) {
+                totalWords[j] = totalWords[j].substring(1);
             }
             //Diferent characters ’ and ' that are used for the same
-            if(TotalWords[j].contains("’")) TotalWords[j] = TotalWords[j].replace("’","'");
+            if(totalWords[j].contains("’")) totalWords[j] = totalWords[j].replace("’","'");
             boolean correct = false;
             //Search for the correct word
-            for(int i = 0; i < Words.size(); ++i){
+            for(int i = 0; i < words.size(); ++i){
                 //The checking part distinguish between capital letters, switching to LowerCase letters except "I"
-                if(TotalWords[j].length() > 1 || (TotalWords[j].length() == 1 && !TotalWords[j].equals("I"))) TotalWords[j] =TotalWords[j].toLowerCase();
+                if(totalWords[j].length() > 1 || (totalWords[j].length() == 1 && !totalWords[j].equals("I"))) totalWords[j] = totalWords[j].toLowerCase();
                 //found the word (correct words have $ at the beginning of it) or is a number
-                if((Words.get(i)).equals("$"+TotalWords[j]) ||TotalWords[j].matches("\\d+") ){
+                if((words.get(i)).equals("$"+ totalWords[j]) || totalWords[j].matches("\\d+") ){
                     //Found the word no need to keep looking
                     correct = true;
                     break;
@@ -155,7 +151,7 @@ public class WAnalyzerS{
             }
         }
 	    //calculate probability
-      double probability = (double)misspellingWords/TotalWords.length;
+      double probability = (double)misspellingWords/ totalWords.length;
       return probability;
   }
     
