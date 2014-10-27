a4199511f9decee453e3a66af61d44837805ef82
nandezer <marchf25@gmail.com>
2014-10-27 12:30:49 +0100

    Update WAnalyzerS.java

--- src/main/java/com/github/ai14/prosammgen/WAnalyzerS.java
+++ src/main/java/com/github/ai14/prosammgen/WAnalyzerS.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.regex.Matcher;
